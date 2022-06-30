f06d8114063893c9d8151ff3aaabae6b4a715d34
jaredatandi <jaredatandi07@gmail.com>
2022-06-30 14:52:11 -0700

    update puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
  * main function - prints the line "programming is like building a multilingual puzzle"
- * return: 0 if exited properly, non-zero otherwise 
-**/
+ * return: 0 if succssful, a number if otherwise 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
