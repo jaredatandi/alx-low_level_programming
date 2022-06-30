e8dea45a14e4eb204d5d789b35405fab02a23a02
jaredatandi <jaredatandi07@gmail.com>
2022-06-30 15:13:36 -0700

    update size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of an int: %zu bytes(s)\n", sizeof(int));
 	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
-	printf("Size of a double: %zu bytes(s)\n", sizeof(double));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
 	return (0);
 }
