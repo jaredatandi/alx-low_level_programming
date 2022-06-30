5e4e421e141aed309eed850db6b80ec37753d58a
jaredatandi <jaredatandi07@gmail.com>
2022-06-30 14:13:28 -0700

    add puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main function - prints the line "programming is like building a multilingual puzzle"
+ * no arguements
+ * returns 0
+ * */
+
+int mai(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
