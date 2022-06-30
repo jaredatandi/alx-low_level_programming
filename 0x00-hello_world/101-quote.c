367f808b946b8362b6c961b3817262ca03c36ad5
jaredatandi <jaredatandi07@gmail.com>
2022-06-30 14:48:29 -0700

    add quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error.
+ * Return: 0 if successful
+ **/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
