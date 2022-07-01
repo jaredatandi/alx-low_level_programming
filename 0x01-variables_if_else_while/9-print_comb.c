895201dc8d48031f9941635a361cc2daada6f182
jaredatandi <jaredatandi07@gmail.com>
2022-07-01 12:50:25 -0700

    add print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of single digit number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
