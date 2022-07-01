b6a8ad4a864409aefe12448314d8227b62a64be8
jaredatandi <jaredatandi07@gmail.com>
2022-07-01 11:31:09 -0700

    add print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase
+ * alphabet
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
