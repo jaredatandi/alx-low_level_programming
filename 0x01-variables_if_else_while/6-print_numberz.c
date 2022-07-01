3cb0db5e31446f99a4b489b90acc7b150dd89610
jaredatandi <jaredatandi07@gmail.com>
2022-07-01 12:06:17 -0700

    add 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers
+ * using putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar('\n');
+	}
+	putchar('\n');
+	return (0);
+}
