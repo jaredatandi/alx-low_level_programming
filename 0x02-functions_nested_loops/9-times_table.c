c14d21d4a1f5ff18ca89d6b75333be1a8304a6e8
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 20:13:55 +0300

    update times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * time_table - prints multiplication table
+ * times_table - prints multiplication table
  * for 9
  * Return: void
  */
 void times_table(void)
 {
-	int a = 9;
+	int a = 0;
 
-	while (a == 9)
+	while (a < 9)
 	{
 		int b = 0;
 
 		while (b < 10)
 		{
-			_putchar(a + '0');
+			_putchar(9 + '0');
 			_putchar('X');
 			_putchar(b + '0');
 			_putchar('\n');
 		}
+		a++;
 	}
 }
