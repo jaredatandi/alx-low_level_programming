c09fa03270433573934f79b861bfb47d262e884a
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 16:45:18 +0300

    add abs function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - gives absolute value of
+ * @number: inputted integer
+ * an integer
+ * Return: int if positive
+ * abs value if negative
+ */
+int _abs(number)
+{
+	if (number > 0)
+		return (number);
+	return (-number);
+}
