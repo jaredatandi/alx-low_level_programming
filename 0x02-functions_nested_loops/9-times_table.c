9152ef136ef0a87e6cd5ae2ae084deedd52d796d
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 20:09:00 +0300

    add time table for 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * time_table - prints multiplication table
+ * for 9
+ * Return: void
+ */
+void time_table(void)
+{
+	int a = 9, b;
+
+	while (a == 9)
+	{
+		int b = 0;
+
+		while (b < 10)
+		{
+			_putchar(a + '0');
+			_putchar('X');
+			_putchar(b + '0');
+			_putchar('\n');
+		}
+	}
+}
