5eaae0bc92db41ea071bd9059fac2f8a834c91b8
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 16:27:42 +0300

    add signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a
+ * digit
+ * @n: the inputted digit
+ * Return: 0 if 0, -1 if negative
+ * and 1 if positive
+ */
+int print_sign(int n)
+{
+	int n;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n > 0)
+	{
+		return (1);
+	}
+	if (n < 1)
+	{
+		return (-1);
+	}
+}
