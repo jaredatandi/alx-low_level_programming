102993eee2381ca62359bf128179935f36a37800
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 14:15:20 +0300

    update putchar function

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes a character to stdout
- * @a: The inputted character
- * Return: 1 (Success)
- * Return: -1 (Fail)
+ * _putchar - puts a character to the
+ * screen
+ * Return: 1 (success)
+ * if not, -1 is returned
  */
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
