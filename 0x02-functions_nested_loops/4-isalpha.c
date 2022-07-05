6d18c16dd636b3a5008b8000d7866be2f01501f2
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 16:17:06 +0300

    add isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if the input is
+ * a character
+ * @c: character to check
+ * Return: 1 if true, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'));
+}
