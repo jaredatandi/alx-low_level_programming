8f07b61fb812d72560c68fce13c496ec434b71f7
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 21:54:14 +0300

    add 103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints fibonacci not more than
+ * 4000000
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float fibsum;
+
+	while (true)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			fibsum += sum;
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", fibsum);
+
+	return (0);
+}
