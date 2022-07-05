97d83785052cbf308150bbf7a5adb0971542eeea
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 16:36:46 +0300

    update sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,17 @@ int print_sign(int n)
 {
 	if (n == 0)
 	{
+		_putchar(48);
 		return (0);
 	}
 	if (n > 0)
 	{
-		return (1);
+		_putchar('+');
+		return (+1);
 	}
-	return (-1);
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
