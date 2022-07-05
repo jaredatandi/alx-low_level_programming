8b6b3b3819f6bd71bef95112eff5f0de681aaa98
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 19:58:30 +0300

    update 8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,8 +13,8 @@ void jack_bauer(void)
     {
         for (min = 0; min <= 59; min++ )
         {
-            _putchar((hr / 10) + '0');
             _putchar((hr % 10) + '0');
+            _putchar((hr / 10) + '0');
             _putchar(':');
 
             _putchar((min % 10) + '0');
