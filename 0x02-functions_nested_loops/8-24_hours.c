07f3323ea886651ce4937247be9540f391194427
jaredatandi <jaredatandi07@gmail.com>
2022-07-05 19:49:01 +0300

    add 8-24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints 24 hours
+ * minute by minute
+ * Return: void
+ */
+void jack_bauer(void)
+{
+    int hr,min;
+
+    for (hr == 0;hr <= 23; hr++)
+    {
+        for (min == 0; min <= 59; min++ )
+        {
+            _putchar((hr / 10) + '0');
+            _putchar((hr % 10) + '0');
+            _putchar(':');
+            
+            _putchar((min % 10) + '0');
+            _putchar((min / 10) + '0');
+            _putchar(('\n'));
+        }
+    }
+}
