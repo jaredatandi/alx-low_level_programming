0498c52db664484b1aacb24914cc4561eb38bd38
jaredatandi <jaredatandi07@gmail.com>
2022-07-07 16:58:38 +0300

    update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,12 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+
+		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
 		}
