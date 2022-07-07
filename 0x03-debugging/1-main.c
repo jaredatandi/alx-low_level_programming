b482aa5689dc71db0474b465cf2813ae20eb7dc6
jaredatandi <jaredatandi07@gmail.com>
2022-07-07 09:46:43 +0300

    update 1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	int i;
+	 int i;
 
 	printf("Infinite loop incoming :(\n");
 
@@ -15,10 +15,10 @@ int main(void)
 
 	/*
 	 * while (i < 10)
+	 */
 	{
 		putchar(i);
 	}
-	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
