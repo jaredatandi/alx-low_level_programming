07c6e57596a899c856f177f90a36052e3c6fb784
jaredatandi <jaredatandi07@gmail.com>
2022-07-12 21:20:58 +0300

    add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - changes the value of a pointer to 98
+ * @n: inputted pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a directory for pointers and memory management
