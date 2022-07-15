6428cb00772eb0b0501e646525366c986ca58895
jaredatandi <jaredatandi07@gmail.com>
2022-07-15 21:03:53 +0300

    update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 == s2)
-		return (0);
-	if (s1 > s2)
-		return (s1[0] - s2[0]);
-	else
-		return (s1[0] - s2[0]);
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
