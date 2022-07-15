1750ad05e61b2bad602f1eb3c49632f7182329a8
jaredatandi <jaredatandi07@gmail.com>
2022-07-15 20:53:32 +0300

    add 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: positive int if s1 > s2
+ * negative int if s2 > s1 and
+ * 0 if they are equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+		return (0);
+	if (s1 > s2)
+		return (s1 - s2);
+	else
+		return (s1 - s2);
+}
