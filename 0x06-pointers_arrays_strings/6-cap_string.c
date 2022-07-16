8162e7d90b50d675f9ab0baea180208c303be3f0
jaredatandi <jaredatandi07@gmail.com>
2022-07-16 09:13:55 +0300

    add 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitilize each word
+ * @word: each word
+ * Return: a pointer to the string
+ */
+char *cap_string(char *word)
+{
+	int i, j;
+
+	char speChar[13] = {'.', ',', ';', ' ',
+		'\n', '\t', '"', '!', '?', '(', ')', '{', '}'};
+
+	for (i = 0; word[i] != '\0'; i++)
+	{
+		if (i == 0 && word[i] >= 'a' && word[i] <= 'z')
+			word = word - 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (word[i] == speChar[j])
+			{
+				if (word[i + 1] >= 'a' && word[i + 1] <= 'z')
+				{
+					word[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (word);
+
+}
