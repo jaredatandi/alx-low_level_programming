5eccbb75510de42fa5ccf904b80473d8ee01d516
jaredatandi <jaredatandi07@gmail.com>
2022-07-18 20:51:09 +0300

    update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,10 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return (&s[i]);
+	while (s[i] != '\0' && s[i] != c)
 		i++;
-	}
+	if (s[i] == c)
+		return (&s[i]);
+
 	return (0);
 }
