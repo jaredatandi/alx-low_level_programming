7c9f66d6f96ef4283b2b960d54b6e51dd70ad0cd
jaredatandi <jaredatandi07@mail.com>
2022-07-22 15:42:15 +0300

    remove trailing whitespace

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - prints the number of arguments passed
  * @argc: counts the arguments to main
  * @argv: pointer to array of pointers
- * Return: 0 if success 
+ * Return: 0 if success
  */
 
 int main(int argc, __attribute__((unused))char *argv[])
