177335a32be38bcf021a07eb98d493c0739b6b93
jaredatandi <jaredatandi07@mail.com>
2022-07-22 17:20:57 +0300

    add 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,18 +6,35 @@
  * @argv: pointer to argument arrays
  * Return: 0 (success)
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i;
-	int res[100];
+	int i, j, num, res, k;
 
-	if (argc == 1)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error\n");
+
+				return (1);
+			}
+		}
 	}
 
+	res = 0;
+
+	for (k = 0; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+
+		if (num >= 0)
+		{
+			res += num;
+		}
+	}
+	printf("%d\n", res);
 
 	return (0);
-
-}	
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int _putchar(char c);
 
