f0a5422533e660fc2e47861ecad8988facbc5ce4
jaredatandi <jaredatandi07@mail.com>
2022-07-22 15:40:50 +0300

    change the return type of 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,12 +4,12 @@
  * main - prints the number of arguments passed
  * @argc: counts the arguments to main
  * @argv: pointer to array of pointers
- * Return: the number of arguments
+ * Return: 0 if success 
  */
 
 int main(int argc, __attribute__((unused))char *argv[])
 {
 	printf("%d\n", argc - 1);
 
-	return (argc);
+	return (0);
 }
