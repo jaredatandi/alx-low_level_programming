511d58a2484bb2e0632c64977f74d34abe85c25d
jaredatandi <jaredatandi07@mail.com>
2022-07-22 15:25:07 +0300

    add 0-whatsmyname and gitignore

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * progname - prints the name of this file
+ * @argc: the argument count
+ * @argv: argument vector
+ * Return: nothing 
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+
+	return (0);
+}
