39e0657fae02ae6a2634d101bd288b87ca79c330
jaredatandi <jaredatandi07@mail.com>
2022-07-22 16:51:05 +0300

    changed 3-mul to accommodate no input given

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
 	int a, b, res;
 
-	if (argc == 1)
+	if (argc == 1 || argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 
@@ -23,12 +23,6 @@ int main(int argc, char **argv)
 	b = atoi(argv[2]);
 	
 	res = a * b;
-
-	if (argc < 3  || argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
 
 	printf("%d\n", res);
 
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: pointer to argument arrays
+ * Return: 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int res[100];
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+
+	return (0);
+
+}	
