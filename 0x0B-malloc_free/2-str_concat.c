8a46b678fcef61d8f7aa69c92c1b0bf8b2d22ad8
jaredatandi <jaredatandi07@mail.com>
2022-07-25 20:20:34 +0300

    fixed trailing statements for betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,20 +15,21 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		len1 = 0;
-	else for (i = 0; s1[i] != '\0'; i++)
-	{
-		len1++;
-	}
+	else
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			len1++;
+		}
 
 	if (s2 == NULL)
 		len2 = 0;
-	else for (i = 0; s2[i] != '\0'; i++)
-	{
-		len2++;
-	}
+	else
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			len2++;
+		}
 
 	total_len = len1 + len2 + 1;
-
 	string = malloc(sizeof(char) * total_len);
 
 	if (string == NULL)
