c4cf782cc8e99b50736ba1d0cabab5a51c139806
jaredatandi <jaredatandi07@mail.com>
2022-07-25 16:21:06 +0300

    update 0-create_array to account for null pointer and size 0 when allocating memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,11 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *) malloc(size * sizeof(int));
 
+	if (ptr == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
