8e39340875c8a72b4d542a76683ec4500e477205
jaredatandi <jaredatandi07@mail.com>
2022-07-29 10:25:48 +0300

    remove std strlen

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,30 +16,25 @@ void *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	i = len1 = len2 = 0;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
-	if (s1 == NULL)
-		len1 = 0;
-	if (s2 == NULL)
-		len2 = 0;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
 	if (n < len2)
 		str = malloc(sizeof(char) * (1 + n + len1));
 	else
 		str = malloc(sizeof(char) * (1 + len1 + len2));
 
-	if (str == NULL)
+	if (!str)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
 
 	if (n < len2)
-	{
 		for (i = 0; i < (len1 + n); i++)
 			str[len1 + i] = s2[i];
-	}
 	else
 		for (i = 0; i < (len1 + len2 + 1); i++)
 			str[len1 + i] = s2[i];
