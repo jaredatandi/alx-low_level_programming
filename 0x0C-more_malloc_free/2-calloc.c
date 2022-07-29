c61351951eb0ca38922ff3013145635d95334c21
jaredatandi <jaredatandi07@mail.com>
2022-07-29 10:57:36 +0300

    update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
 
-	if ((nmemb == 0) | (size == 0))
+	if ((nmemb == 0) || (size == 0))
 		return (NULL);
 
 	arr = malloc(sizeof(char) * nmemb);
