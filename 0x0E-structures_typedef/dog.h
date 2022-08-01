33d7f8fb9a4952e4e42ee62c41763fc0cb672032
jaredatandi <jaredatandi07@mail.com>
2022-08-01 16:29:04 +0300

    add readme and dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - creates a new type dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
