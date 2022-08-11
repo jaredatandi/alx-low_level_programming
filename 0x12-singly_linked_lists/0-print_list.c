29a9fc5905ffb7c946d2e2a0bc221101206493d6
jaredatandi <jaredatandi07@gmail.com>
2022-08-11 21:20:00 +0300

    include \n to print a new line after nil

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			_puts("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
