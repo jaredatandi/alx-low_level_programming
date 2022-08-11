83ba520be42c216178834dc93ff1be9b0a32172d
jaredatandi <jaredatandi07@gmail.com>
2022-08-11 22:14:00 +0300

    create a function to add to the last node

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - adds a node at the end of a
+ * linked list
+ * @head: a pointer to the struct list_t
+ * @str: the string to add
+ *
+ * Return: address to the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+
+
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -34,6 +34,7 @@ size_t list_len(const list_t *h);
 
 /******************* add a node ******************************/
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 
 
