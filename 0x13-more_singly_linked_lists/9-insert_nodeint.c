7a0accdabfc39b2595b8365ef6927b352557a340
jaredatandi <jaredatandi07@gmail.com>
2022-08-17 00:27:07 +0300

    update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,15 +27,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (temp)
 	{
-		temp = temp->next;
 		count++;
 
-		if (count == idx - 1)
+		if (count == idx)
 		{
 			newnode->next = temp->next;
 			temp->next = newnode;
+
 			return (newnode);
 		}
+
+		temp = temp->next;
 	}
+
 	return (NULL);
 }
