3a73d2a35e52b3b466625064f61b4dab5f97c333
jaredatandi <jaredatandi07@gmail.com>
2022-08-16 23:13:05 +0300

    add 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns nth node of a listint_t
+ * @head: pointer to listint_t
+ * @index: desired index
+ * Return: the nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t count = 0;
+	listint_t *temp;
+
+	temp = head;
+
+	while (temp)
+	{
+		temp = temp->next;
+		count++;
+		if (count == index)
+			return (temp);
+	}
+	return (NULL);
+}
