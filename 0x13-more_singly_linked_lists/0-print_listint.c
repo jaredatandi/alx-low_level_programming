2673cf4f1643dc4337931e6ef0d7910cb6069a30
jaredatandi <jaredatandi07@gmail.com>
2022-08-16 18:14:39 +0300

    write display function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements of a listint_t
+ * @h: a pointer to listint_t
+ *
+ * Description: display all the elements and count them
+ *
+ * Return: unsigned int count of all the elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (!h)
+		return (0);
+	else
+		while (h)
+		{
+			printf("%d\n", h->n);
+			return (print_listint(h->next) + 1);
+		}
+	return (0);
+}
