1795d1ba8190b9eaf9fc648588814f58aedf7105
jaredatandi <jaredatandi07@gmail.com>
2022-08-18 15:03:19 +0300

    add 0-binary_to_unit.c

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * binary_to_unit - converts a binary number to unsigned int
+ * @b: a string of 0s and 1s
+ *
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int i;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		decimal = 2 * decimal + (b[i] - '0'); 
+	}
+
+	return (decimal);
+}
+/**
+ * power - calculates base raised to any power
+ * @num: power factor
+ * @base: the base number
+ * Return: the 2 power num
+ */
+
+unsigned int power(unsigned int base, unsigned int num)
+{
+	unsigned int result = 1;
+
+	if (num == 0)
+		return (result);
+
+	for (num = num; num > 0; num--)
+		result = result * base;
+	return (result);
+}
