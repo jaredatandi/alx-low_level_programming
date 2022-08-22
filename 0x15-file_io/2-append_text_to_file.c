90edc43b848f08973d86a0650f663ff74ac5b14d
jaredatandi <jaredatandi07@gmail.com>
2022-08-23 00:17:06 +0300

    update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,18 +11,17 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, lenr;
-	size_t buff;
+	ssize_t buff;
 
-	for (buff = 0; text_content[buff]; buff++) 
+	for (buff = 0; text_content[buff]; buff++)
 		;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd < 0)
+	if (fd == 0)
 	{
-		close (fd);
 		return (-1);
 	}
 	if (text_content != NULL)
@@ -30,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	close(fd);
 
-	if (lenr < 0)
+	if (lenr == 0)
 		return (-1);
 
 	return (1);
