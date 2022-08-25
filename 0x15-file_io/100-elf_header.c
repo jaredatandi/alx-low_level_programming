caf3208bdaad7f3d54c4ff1b38bbe3664d61bb45
jaredatandi <jaredatandi07@gmail.com>
2022-08-26 00:54:38 +0300

    add the other functions

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,6 +11,136 @@ int check_elf(unsigned char *e_ident);
 void close_elf(int elf);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_ABIVersion(unsigned char *e_ident);
+void print_entry(unsigned char e_entry, unsigned char *e_ident);
+
+
+/**
+ * print_entry - file ABI version
+ * @e_ident: array of bytes that specifies
+ * @e_entry: the __libstart address
+ * how to interpret the file
+ */
+
+void print_entry(unsigned char e_entry, unsigned char *e_ident)
+{
+	printf("Entry point address:		");
+	if (e_ident[4] == 2)
+	{
+		printf("0x%x\n", e_entry);
+	}
+	else
+		printf("0x%x\n", e_entry);
+}
+
+/**
+ * print_ABIVersion - file ABI version
+ * @e_ident: array of bytes that specifies
+ * how to interpret the file
+ */
+
+void print_ABIVersion(unsigned char *e_ident)
+{
+	printf("ABI Version:			");
+	printf("%d\n", e_ident[EI_ABIVERSION]);
+}
+/**
+ * print_osabi - the file OS/ABI
+ * @e_ident: array of bytes that specifies
+ * how to interpret the file
+ */
+void print_osabi(unsigned char *e_ident)
+{
+	printf("OS/ABI:			");
+	switch (e_ident[EI_OSABI])
+	{
+		case ELFOSABI_NONE:
+			printf("Unix - system\n");
+			break;
+		case ELFOSABI_HPUX:
+			printf("HP-UX ABI\n");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("NetBSD ABI\n");
+			break;
+		case ELFOSABI_LINUX:
+			printf("Linux ABI\n");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("Solaris ABI\n");
+			break;
+		case ELFOSABI_IRIX:
+			printf("IRIX ABI\n");
+			break;
+		case ELFOSABI_FREEBSD:
+			printf("FreeBSD ABI\n");
+			break;
+		case ELFOSABI_TRU64:
+			printf("TRU64 UNIX ABI\n");
+			break;
+		case ELFOSABI_ARM:
+			printf("ARM architecture ABI\n");
+			break;
+		case ELFOSABI_STANDALONE:
+			printf("Stand-alone (embedded) ABI\n");
+			break;
+		default:
+			printf("Invalid\n");
+			break;
+	}
+}
+
+/**
+ * print_version - the file version
+ * @e_ident: array of bytes that specifies
+ * how to interpret the file
+ */
+void print_version(unsigned char *e_ident)
+{
+	printf("Version:		");
+	switch (e_ident[EI_VERSION])
+	{
+		case EV_NONE:
+			printf("Invalid version\n");
+			break;
+		case EV_CURRENT:
+			printf("Current version\n");
+			break;
+		default:
+			printf("<Unknown version %x\n", e_ident[EI_VERSION]);
+	}
+}
+
+
+/**
+ * print_data - the file data
+ * @e_ident: array of bytes that specifies
+ * how to interpret the file
+ */
+
+void print_data(unsigned char *e_ident)
+{
+	printf("Data:			");
+	switch (e_ident[EI_DATA])
+	{
+		case ELFDATANONE:
+			printf("Unknown data format\n");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little-endian\n");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big-endian\n");
+			break;
+		default:
+			printf("<Unknown data format %x>\n", e_ident[EI_DATA]);
+			break;
+	}
+}
+
 
 /**
  * print_class - the file class
@@ -23,18 +153,18 @@ void print_class(unsigned char *e_ident)
 	printf("Class:			");
 	switch (e_ident[EI_CLASS])
 	{
-	case ELFCLASSNONE:
-		printf("invalid class\n");
-		break;
-	case ELFCLASS32:
-		printf("ELF32\n");
-		break;
-	case ELFCLASS64:
-		printf("ELF64\n");
-		break;
-	default:
-		printf("<Unknown: %x>", e_ident[EI_CLASS]);
-		break;
+		case ELFCLASSNONE:
+			printf("invalid class\n");
+			break;
+		case ELFCLASS32:
+			printf("ELF32\n");
+			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("<Unknown: %x>", e_ident[EI_CLASS]);
+			break;
 	}
 }
 
@@ -143,6 +273,11 @@ int main(int argc, char **argv)
 	check_elf(header->e_ident);
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_ABIVersion(header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
 	return (argc);
 }
