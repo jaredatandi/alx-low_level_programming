35c166e36f41261a364d59e18925262ff092ff39
jaredatandi <jaredatandi07@gmail.com>
2022-10-22 22:51:15 +0300

    Get function

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table to look into
+ * @key: the key you are look for
+ *
+ * Return: the value associated with teh element
+ * NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (key == NULL || ht == NULL)
+		return (NULL);
+	
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	for (;node != NULL; node = node->next)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+	}
+	return (NULL);
+}
