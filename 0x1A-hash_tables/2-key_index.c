fd30a1b9e214f0addc585c0b1683a9247e5469e3
jaredatandi <jaredatandi07@gmail.com>
2022-10-22 18:52:09 +0300

    Fix betty doc errors

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,8 +4,10 @@
  * key_index - generates the key using the djb2 algo
  * @key: the hash key
  * @size: size of the array table
- * @Return: index of the key
+ *
+ * Return: index of the key
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
